b2d815debf9a15e12df6eb1b0c0b1a2ac21876b8
JonathanRN <minijona7@gmail.com>
2018-10-19 13:22:20 -0400

    small fix to email validation

--- src/emailValidator.js
+++ src/emailValidator.js
@@ -2,8 +2,7 @@
 class EmailValidator {
     isEmailValid(email) {
         let regex = new RegExp("^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$");
-        let found = email.match(regex);
-        return (found == true);
+        return email.match(regex);
     }
 }
 
